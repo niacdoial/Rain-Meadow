c52019b74665f0ca5462f64ecbf9a333d125ae77
invalidunits <invalidunits@gmail.com>
2025-08-07 21:08:27 -0400

    Make JoinLobbyPacket Reliable

--- Online/Domain/Packets/RequestJoinPacket.cs
+++ Online/Domain/Packets/RequestJoinPacket.cs
@@ -39,7 +39,7 @@ namespace RainMeadow
                     OnlineManager.players.Count,
                     RainMeadowModManager.ModArrayToString(RainMeadowModManager.GetRequiredMods()),
                     RainMeadowModManager.ModArrayToString(RainMeadowModManager.GetBannedMods())
-                ), UDPPeerManager.PacketType.Unreliable);
+                ), UDPPeerManager.PacketType.Reliable);
 
             }
         }
