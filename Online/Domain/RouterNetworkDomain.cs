a8400f25e2bc30713a5772c56caaf1881d324656
invalidunits <invalidunits@gmail.com>
2025-08-18 01:52:56 -0400

    Router Domain (unfinished)

---
+++ Online/Domain/RouterNetworkDomain.cs
@@ -0,0 +1,261 @@
+using System;
+using System.Net;
+using System.Linq;
+using System.IO;
+using Menu;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using RainMeadow.Shared;
+
+
+namespace RainMeadow
+{
+
+    public partial class RouterNetworkDomain : NetworkDomain
+    {
+        public class RouterLobbyInfo : LobbyInfo
+        {
+            public IPEndPoint endPoint;
+            public RouterLobbyInfo(IPEndPoint endPoint, string name, string mode, int playerCount, bool hasPassword, int maxPlayerCount, string highImpactMods = "", string bannedMods = "") :
+                base(name, mode, playerCount, hasPassword, maxPlayerCount, highImpactMods, bannedMods)
+            {
+                this.endPoint = endPoint;
+            }
+            public override string GetLobbyJoinCode(string? password = null)
+            {
+                if (password != null)
+                    return $"+connect_lan_lobby {endPoint.Address.Address} {endPoint.Port} +lobby_password {password}";
+                return $"+connect_lan_lobby {endPoint.Address.Address} {endPoint.Port}";
+            }
+        }
+
+        public class RouterPlayerId : MeadowPlayerId
+        {
+            // TODO IPEndpoint and NAT stuff
+            public ushort routingID;
+            public RouterPlayerId(ushort routingID) : base(
+                    UsernameGenerator.GenerateRandomUsername(routingID))
+            {
+                this.routingID = routingID;
+            }
+
+            public override void OpenProfileLink()
+            {
+                string dialogue = "RoutingID: " + routingID.ToString();
+                OnlineManager.instance.manager.ShowDialog(
+                    new DialogNotify(dialogue, new Vector2(478.1f, 115.200005f * (1 + 0.2f * UDPPeerManager.getInterfaceAddresses().Length)),
+                        OnlineManager.instance.manager, null));
+            }
+
+            public override int GetHashCode()
+            {
+                return routingID;
+            }
+
+            public override void CustomSerialize(Serializer serializer)
+            {
+                serializer.Serialize(ref routingID);
+            }
+
+            public override bool Equals(MeadowPlayerId other)
+            {
+                if (other is RouterPlayerId id)
+                {
+                    return routingID == id.routingID;
+                }
+
+                return false;
+            }
+        }
+        
+        public override OnlinePlayer CreateMePlayer()
+        {
+            return new OnlinePlayer(new RouterPlayerId(ushort.MaxValue)
+                { name = RainMeadow.rainMeadowOptions.LanUserName.Value })
+                { isMe = true };
+        }
+
+
+        static List<RouterLobbyInfo> lobbyinfo = new();
+        public override void RequestLobbyList()
+        {
+            lobbyinfo.Clear();
+        }
+
+
+        public OnlinePlayer? GetPlayerRouter(ushort routingID, bool create = false)
+        {
+            var player = OnlineManager.players.FirstOrDefault(p =>
+            {
+                if (p.id is RouterPlayerId route)
+                    if (route.routingID != ushort.MaxValue) return route.routingID == routingID;
+                return false;
+            });
+
+            if (player is null && create)
+            {
+                RainMeadow.Debug($"Couldn't find player with endpoint {routingID}. Creating one...");
+                player = new OnlinePlayer(new RouterPlayerId(routingID));
+            }
+
+            return player;
+        }
+
+        public override bool canSendChatMessages => false; // TODO: Chat Messages in router domain
+        public override void SendChatMessage(string message)
+        {
+            return;
+        }
+
+        public override void CreateLobby(LobbyVisibility visibility, string gameMode, string? password, int? maxPlayerCount)
+        {
+            // maxplayercount = maxPlayerCount ?? 0;
+            // OnlineManager.lobby = new Lobby(new OnlineGameMode.OnlineGameModeType(gameMode), OnlineManager.mePlayer, password);
+            // NetworkDomain.OnLobbyJoinedEvent(true, "");
+        }
+
+        public void LobbyAcknoledgedUs(OnlinePlayer owner)
+        {
+            RainMeadow.DebugMe();
+            if (OnlineManager.lobby is null)
+            {
+                OnlineManager.lobby = new Lobby(
+                    new OnlineGameMode.OnlineGameModeType(OnlineManager.currentlyJoiningLobby.mode, false),
+                    owner, lobbyPassword);
+            }
+
+        }
+
+
+        public void AcknoledgeRouterPlayer(OnlinePlayer joiningPlayer)
+        {
+            RainMeadow.DebugMe();
+            if (OnlineManager.players.Contains(joiningPlayer)) { return; }
+            OnlineManager.AddPlayer(joiningPlayer);
+            RainMeadow.Debug($"Added {joiningPlayer} to the lobby matchmaking player list");
+            OnPlayerListReceivedEvent(OnlineManager.players.Select(x => x.id).ToArray());
+        }
+
+        public void RemoveLANPlayer(OnlinePlayer leavingPlayer)
+        {
+            StackTrace stackTrace = new();
+            RainMeadow.Debug(stackTrace.ToString());
+
+
+            if (leavingPlayer.isMe) return;
+            if (!OnlineManager.players.Contains(leavingPlayer)) { return; }
+            OnlineManager.RemovePlayer(leavingPlayer);
+            ForgetPlayer(leavingPlayer);
+            OnPlayerListReceivedEvent(OnlineManager.players.Select(x => x.id).ToArray());
+        }
+        string lobbyPassword = "";
+        public override void RequestJoinLobby(LobbyInfo lobby, string? password)
+        {
+            RainMeadow.DebugMe();
+            if (lobby is  lobbyinfo)
+            {
+                lobbyPassword = password ?? "";
+                OnlineManager.currentlyJoiningLobby = lobby;
+                var lobbyInfo = (LANLobbyInfo)lobby;
+                if (lobbyInfo.endPoint == null)
+                {
+                    RainMeadow.Debug("Failed to join local game...");
+                    return;
+                }
+
+                RainMeadow.Debug("Sending Request to join lobby...");
+                SendP2P(new OnlinePlayer(new LANPlayerId(lobbyInfo.endPoint)),
+                    new RequestJoinPacket(OnlineManager.mePlayer.id.name), UDPPeerManager.PacketType.Reliable, true);
+            }
+            else
+            {
+                RainMeadow.Error("Invalid lobby type");
+            }
+        }
+
+        public override void JoinLobby(bool success)
+        {
+            if (success)
+            {
+                RainMeadow.Debug("Joining lobby");
+                OnLobbyJoinedEvent(true);
+            }
+            else
+            {
+                OnlineManager.LeaveLobby();
+                RainMeadow.Debug("Failed to join local game. Wrong Password");
+                OnLobbyJoinedEvent(false, Utils.Translate("Wrong password!"));
+            }
+        }
+
+        public override void JoinLobbyUsingArgs(params string?[] args)
+        {
+            if (args.Length >= 2 && long.TryParse(args[0], out var address) && int.TryParse(args[1], out var port))
+            {
+                RainMeadow.Debug($"joining lobby with address {address} and port {port} from the command line");
+                RequestJoinLobby(new LANLobbyInfo(new IPEndPoint(address, port), "", "", 0, false, 4), args.Length > 2 ? args[2] : null);
+            }
+            else
+                RainMeadow.Error($"invalid address and port: {string.Join(" ", args)}");
+        }
+
+        public override void HandleLeavingLobby()
+        {
+            if (OnlineManager.players is not null)
+            {
+                if (OnlineManager.players.Count > 1)
+                {
+                    foreach (OnlinePlayer p in OnlineManager.players)
+                    {
+                        SendP2P(p,
+                            new SessionEndPacket(),
+                                UDPPeerManager.PacketType.Unreliable);
+                    }
+                }
+            }
+            ForgetEverything();
+        }
+
+        public override OnlinePlayer? GetLobbyOwner()
+        {
+            if (OnlineManager.lobby == null) return null;
+            if (OnlineManager.lobby.owner is null || OnlineManager.lobby.owner.hasLeft)
+            {
+                // select a new owner. 
+                // The order of players should be 
+                for (int i = 0; i < OnlineManager.players.Count; i++)
+                {
+                    OnlinePlayer onlinePlayer = OnlineManager.players[i];
+                    if (onlinePlayer.hasLeft) continue;
+                    return onlinePlayer;
+                }
+
+                return null;
+            }
+
+            return OnlineManager.lobby.owner;
+        }
+
+        public override MeadowPlayerId GetEmptyId()
+        {
+            return new LANPlayerId(null);
+        }
+
+        public override string GetLobbyID()
+        {
+            if (OnlineManager.lobby != null)
+            {
+                return (OnlineManager.lobby.owner.id as LANPlayerId)?.GetPersonaName() ?? Utils.Translate("Nobody");
+            }
+
+            return "Unknown Lan Lobby";
+        }
+        public override void OpenInvitationOverlay()
+        {
+            OnlineManager.instance.manager.ShowDialog(new DialogNotify(Utils.Translate("You cannot use this feature here."), OnlineManager.instance.manager, null));
+        }
+
+        public override bool canOpenInvitations => false;
+    }
+}
